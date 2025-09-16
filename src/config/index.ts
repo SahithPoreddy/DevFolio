a098e90ddc9412cea3b9dfa7c88b53cf84b56941
sahithporeddy <sahith12poreddy@gmail.com>
2025-09-16 18:05:07 +0530

    Done

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,97 +1,87 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Sahith Chandra Poreddy — Web Developer",
+  author: "Sahith Chandra Poreddy",
   description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+    "Software Engineer based in Hyderabad, India. I specialize in MERN stack development.",
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/public/SahithChandraPoreddy.JPG",
   navLinks: [
     { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://www.linkedin.com/in/sahith-chandra-poreddy-1b37b3242/" },
+    { text: "Github", href: "https://github.com/SahithPoreddy" },
   ],
-  socialImage: "/zen-og.png",
-  canonicalURL: "https://astro-zen.vercel.app",
+  socialImage: "/public/SahithChandraPoreddy.JPG",
+  canonicalURL: "https://github.com/SahithPoreddy",
 };
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Sahith Chandra Poreddy",
+    specialty: "Web Developer",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+      "Developer based in Hyderabad, India. I specialize in MERN stack development.",
+    email: "sahith12poreddy@gmail.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      company: "BluSapphire Cyber Systems Pvt Ltd",
+      position: "Product Developer Intern",
+      startDate: "Sept 2024",
+      endDate: "May 2025",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Coordinated in a team of four to design and implement a threat hunting tool for Windows using Win32 APIs and C++ libraries, enhancing system security by proactively identifying and mitigating cyber threats.",
+        "Tested and improved existing EDR software by using the concept of entropy to improve the effectiveness of the software on both Windows and Linux O.S.",
+        "Developed and Tested a logger & shipper module for the EDR software using filebeat."
       ],
     },
     {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
-      summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
-      ],
-    },
-    {
-      company: "Driveer",
+      company: "Geethanjali College of E&T",
       position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
+      startDate: "Feb 2023",
+      endDate: "May 2023",
+      summary: [
+        "Designed and developed a full stack web application using React, Express and MySQL to aid exam branch of the college, enabling complete digitization of the exam registration process.",
+        "The web app significantly boosted the registration process and also allowed for centralization of students data. This led to a 30% reduction in manual work for the exam branch staff.",
+        "Implemented responsive design principles to ensure the application was accessible and user-friendly across various devices, enhancing the overall user experience.",
+      ],
     },
   ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/spotifu.png",
+      name: "Chatbot Builder",
+      summary: "A flowchart-based logic builder which can be used to create chatbots.",
+      linkPreview: "https://chatbot-builder-olive.vercel.app/",
+      linkSource: "https://github.com/SahithPoreddy/Chatbot-builder",
+      image: "/public/Chatbot_builder.png",
     },
-    {
-      name: "Shopp App",
-      summary: "An e-commerce platform that replicates Shopify's key features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/shopify-clon.png",
-    },
-    {
-      name: "ClonTagram",
-      summary: "A social network that replicates the features of Instagram",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/clone-ig.png",
-    },
+    // {
+    //   name: "Shopp App",
+    //   summary: "An e-commerce platform that replicates Shopify's key features.",
+    //   linkPreview: "/",
+    //   linkSource: "https://github.com/immois/astro-zen",
+    //   image: "/shopify-clon.png",
+    // },
+    // {
+    //   name: "ClonTagram",
+    //   summary: "A social network that replicates the features of Instagram",
+    //   linkPreview: "/",
+    //   linkSource: "https://github.com/immois/astro-zen",
+    //   image: "/clone-ig.png",
+    // },
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
+    Hi, I’m Sahith Chandra, a passionate Full Stack Developer and Product Developer with a strong foundation in Cyber Security. As a recent graduate, I’m eager to explore opportunities that challenge me and help me grow into a well-rounded professional.
 
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+    I bring hands-on experience in building user-friendly applications using the MERN stack (MongoDB, Express.js, React, Node.js), along with practical expertise in Endpoint Detection & Response (EDR) solutions for both Windows and Linux environments. My background allows me to bridge the gap between secure software development and innovative product design, ensuring that the applications I build are not only functional but also resilient and secure.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/public/SahithChandraPoreddy.JPG",
   },
 };
 
